deb535a66791d0ba54751adf5e2e1b1b0cc1d2d5
Arman Markov <48671135+CMDRMark@users.noreply.github.com>
2022-11-01 01:15:18 +0400

    Delete src directory

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-function App(props) { return (
-<div className="App">
-<header className="App-header">
-React Message app:
-<h3>{props.message}</h3> </header>
-</div> );
-}
-export default App;
