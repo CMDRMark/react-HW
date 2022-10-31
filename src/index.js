7b1936b9d15028d73ad99ac750b646a7af8b0e9c
Arman Markov <armanmarkov.dev@gmail.com>
2022-11-01 01:09:57 +0400

    Setup of message App

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const Message = 'This is the text of message';
 root.render(
   <React.StrictMode>
-    <App />
+    <App message={Message} />
   </React.StrictMode>
-);
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
